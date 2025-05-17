cf0f127f2b550ad082ab6380e9a978827dcf2889
RIDDHIOZA25 <riddhioza1779@gmail.com>
2025-05-17 15:19:08 -0400

    Router added

--- LL Front End/apartment-listings/src/App.jsx
+++ LL Front End/apartment-listings/src/App.jsx
@@ -9,8 +9,8 @@ import MemberHome from "./pages/MemberHome";
 import DashboardPage from "./pages/DashboardPage";
 import ListingDetailPage from "./pages/ListingDetailPage";
 import ProtectedRoute from "./components/ProtectedRoute";
-import JoinViaInvite from './pages/JoinViaInvite'
-import EmailConfirmed from './pages/EmailConfirmed';
+import JoinViaInvite from "./pages/JoinViaInvite";
+import EmailConfirmed from "./pages/EmailConfirmed";
 
 import PublicLayout from "./layouts/PublicLayout";
 import MemberLayout from "./layouts/MemberLayout";
@@ -18,45 +18,47 @@ import MemberLayout from "./layouts/MemberLayout";
 export default function App() {
   return (
     <>
-      <Navbar />
+      <Router>
+        <Navbar />
 
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/auth" element={<AuthPage />} />
-        <Route
-          path="/home"
-          element={
-            <PublicLayout>
-              <PublicHome />
-            </PublicLayout>
-          }
-        />
-        <Route
-          path="/home/:uid"
-          element={
-            <ProtectedRoute>
-              <MemberLayout>
-                <MemberHome />
-              </MemberLayout>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <MemberLayout>
-                <DashboardPage />
-              </MemberLayout>
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/email-confirmed" element={<EmailConfirmed />} />
-        <Route path="/listing/:id" element={<ListingDetailPage />} />
-        <Route path="/join" element={<JoinViaInvite />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route
+            path="/home"
+            element={
+              <PublicLayout>
+                <PublicHome />
+              </PublicLayout>
+            }
+          />
+          <Route
+            path="/home/:uid"
+            element={
+              <ProtectedRoute>
+                <MemberLayout>
+                  <MemberHome />
+                </MemberLayout>
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <MemberLayout>
+                  <DashboardPage />
+                </MemberLayout>
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/email-confirmed" element={<EmailConfirmed />} />
+          <Route path="/listing/:id" element={<ListingDetailPage />} />
+          <Route path="/join" element={<JoinViaInvite />} />
+        </Routes>
 
-      <AuthModal />
+        <AuthModal />
+      </Router>
     </>
   );
 }
